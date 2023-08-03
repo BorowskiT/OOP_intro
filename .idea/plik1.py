2fcce4914b063ee510ceccbc1460cad5117d01d7
BorowskiT <borowskitomasz5@gmail.com>
2023-08-03 19:22:21 +0200

    Plik1

---
+++ .idea/plik1.py
@@ -0,0 +1,31 @@
+class Biegacz:
+
+    def __init__(self,imie, nazwisko):
+        self.imie = imie
+        self.naziwsko = nazwisko
+        #self.poziom_szczescia = 0
+        #self.suma_dystansow = 0
+
+    def przedstaw_sie(self):
+        return f"{self.imie} {self.naziwsko}"
+
+    def zrob_przebiezke(self, dystans):
+        if self.poprzedni_dystans is not None and dystans > self.poprzedni_dystans:
+            self.poziom_szczescia += 1
+        #self.poprzedni_dystans = dystans
+
+        #ile_pelnych_dziesiatek_poprzednio = self.suma_dystasow // 10
+class Strongman:
+
+    def __init__(self,imie, nazwisko):
+        self.imie = imie
+        self.naziwsko = nazwisko
+        self.poziom_szczescia = 0
+        #self.suma_dystansow = 0
+
+    def przedstaw_sie(self):
+        return f"{self.imie} {self.naziwsko}"
+
+    def podnies_ciezar(self, ciezar):
+        if ciezar > 100:
+            self.poziom_szczescia += 1
